32df5328c71cc6d50eb6d6d0ff557baf53b8f15e
WLeondike <billyleondike@gmail.com>
2020-11-11 17:32:03 -0500

    updated images into own card component, working on css grid for images

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,8 @@
-import axios from "axios";
-
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://api.unsplash.com",
+  baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: "Client-ID AW7AsxtGBAXAPsOjv7okyKFS156L2RV29KDupea5Y9w",
-  }
+    Authorization: 'Client-ID AW7AsxtGBAXAPsOjv7okyKFS156L2RV29KDupea5Y9w',
+  },
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import unsplash from "../api/unsplash";
-import SearchBar from "./SearchBar";
-import ImageList from "./ImageList";
+import React from 'react';
+import unsplash from '../api/unsplash';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList/ImageList';
 
 class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const res = await unsplash.get("/search/photos", {
-      params: { query: term }
+    const res = await unsplash.get('/search/photos', {
+      params: { query: term },
     });
     this.setState({ images: res.data.results });
   };
 
   render() {
     return (
-      <div className='ui container' style={{ marginTop: "10px" }}>
+      <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
         <ImageList images={this.state.images} />
       </div>
